16a792f85297aa1ceb505ec0cb5dad242da8ebac
mashruravi <mashru.ravi@gmail.com>
2017-05-13 12:46:12 +0530

    Add required material design libraries

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+
+import { MaterialModule, MdIconModule } from '@angular/material';
+
+import 'hammerjs';
 
 import { AppComponent } from './app.component';
 
@@ -12,7 +17,10 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+    MdIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
