f72b9446696eabad30c6928ed0a79750b9aae134
Ravi Suresh Mashru <ravi.suresh.mashru@sap.com>
2017-06-03 16:18:50 +0530

    Create Event route animation

---
+++ src/app/animations.ts
@@ -0,0 +1,25 @@
+import { animate, AnimationEntryMetadata, state, style, transition, trigger } from '@angular/core';
+
+// Component transition animations
+export const slideInDownAnimation: AnimationEntryMetadata =
+    trigger('routeAnimation', [
+        state('*',
+            style({
+                opacity: 1,
+                transform: "scale(1)"
+            })
+        ),
+        transition(':enter', [
+            style({
+                opacity: 0,
+                transform: "scale(0)"
+            }),
+            animate('0.2s ease-in')
+        ]),
+        transition(':leave', [
+            animate('0.2s ease-out', style({
+                opacity: 0,
+                transform: "scale(0)"
+            }))
+        ])
+    ]);
--- src/app/components/create-event/create-event.component.ts
+++ src/app/components/create-event/create-event.component.ts
@@ -2,12 +2,19 @@ import { Component, OnInit, HostBinding } from "@angular/core";
 import { EventsService } from "app/services/events.service";
 import { Router } from "@angular/router";
 
+import { slideInDownAnimation } from "app/animations";
+
 @Component({
 	selector: 'app-create-event',
 	templateUrl: './create-event.component.html',
-	styleUrls: ['./create-event.component.css']
+	styleUrls: ['./create-event.component.css'],
+	animations: [slideInDownAnimation]
 })
 export class CreateEventComponent implements OnInit {
+
+	@HostBinding('@routeAnimation') routeAnimation = true;
+
+	@HostBinding('style.position') hostPosition = "absolute";
 
 	private hours: number[] = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23];
 	private minutes: number[] = [0, 15, 30, 45];
